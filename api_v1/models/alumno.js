576320f87a1b2da38be5d1927d0ee6718f84f108
Angel0880 <elhardcase@outlook.com>
2022-06-25 01:05:12 -0300

    API

---
+++ api_v1/models/alumno.js
@@ -0,0 +1,24 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const alumno = sequelize.define('alumno', {
+    nombre: DataTypes.STRING,
+    apellido: DataTypes.STRING,
+    edad: DataTypes.INTEGER,
+    fechaNac: DataTypes.DATE,
+    paisOrigen: DataTypes.STRING,
+    id_carrera: DataTypes.INTEGER
+  }, {});
+  alumno.associate = function(models) {
+    
+    //asociacion a carrera (pertenece a:)
+  	alumno.belongsTo(models.carrera// modelo al que pertenece
+    ,{
+      as : 'Carrera-Relacionada',  // nombre de mi relacion
+      foreignKey: 'id_carrera'     // campo con el que voy a igualar
+    })
+  	/////////////////////
+
+    
+  };
+  return alumno;
+};
